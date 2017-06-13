5c8e08390d92133cbf739596c50d7a6e13ccaba3
William Xiong <wwx@duke.edu>
2017-06-13 00:05:41 -0400

    reorganized shell scripts

---
+++ scripts/rmswp.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+#RUN ONLY AT ROOT OF PROJECT
+cd "$PWD/../" || exit 1
+find . -name '*.swp' -delete
---
+++ scripts/setenv.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+export HOST=localhost
+export PASS=Chem1313#
+export USER=root
+
