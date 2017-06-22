b2a4f36ae71222b30cc2e47b0cbfd5d6d195bb1f
William Xiong <wwx@duke.edu>
2017-06-21 22:13:24 -0400

    node clustering almost works (running on multiple processes) git add .git add .git add .git add . small issue with rendering socketio connection in view

--- app.js
+++ app.js
@@ -17,124 +17,110 @@ const flash = require('connect-flash');
 const expressHandlebars = require('express-handlebars');
 const helmet = require('helmet');
 const cluster = require('cluster');
-//const sticky = require('socketio-sticky-session');
 
 var PORT = process.env.PORT || 3000;
+var HOST = process.env.HOST || 'localhost';
 
-// view engine setup
-
-app.set('views', path.join(__dirname, '/views'));
-
-var handlebars = expressHandlebars.create({ 
-    partialsDir  : [
-        __dirname + '/views/partials'    
-    ]
-});
+//ip hash function
 
 
-app.engine('handlebars', handlebars.engine);
-app.set('view engine', 'handlebars');
-
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-
-app.use(helmet());
-app.use(helmet.hidePoweredBy());
-//TODO include csrf token in frontend input fields
-
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-var sessionMiddleWare = session({
-    secret: crypto.randomBytes(10).toString('hex'),
-    resave: true,
-    saveUninitialized: true,
-    //store : new RedisStore({
-        //host: process.env.HOST,
-        //port: process.env.REDIS_PORT || 6379,
-        //client: cache_store
-    //})
-}); 
-app.use(sessionMiddleWare);
-
-app.use(function(req, res, next) {
-    if(req.session._csrf === undefined) {
-        req.session._csrf = crypto.randomBytes(20).toString('hex');
-    }
-    return next();
-});
-
-http.globalAgent.maxSockets = Infinity;
-
-/* Set up server side socket*/
-
-//handles back button problem of caching- reloads page every time to persist session
-app.use(function(req, res, next) {
-    res.header('Cache-Control', 'no-cache, private, no-store, must-revalidate, max-stale=0, post-check=0, pre-check=0');
-    next();
-});
-
-app.use(flash());
-app.use(passport.initialize());
-app.use(passport.session());
-
-require('./app/authentication/user-pass.js').passportAuth(passport);
-require('./routes/index')(app, passport);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-    var err = new Error('Not Found');
-    err.status = 404;
-    next(err);
-});
-
-// error handler
-app.use(function(err, req, res, next) {
-    // set locals, only providing error in development
-    res.locals.message = err.message;
-    res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-    // render the error page
-    res.status(err.status || 500);
-    res.render('error');
-});
-
-//node clustering for muli threading
-//var numCPUs = require('os').cpus().length;
-
-//if(cluster.isMaster) {
-    //console.log("master process");
-    //for(var i = 0; i < numCPUs; i++) {
-        //cluster.fork();
-    //}
-//}
-//else {
-    //console.log("child process");
-//var options = {
-    //num: 4
-//};
-//var server = sticky(options, function() {
-    //httpServer.listen(PORT);
-    //io.listen(httpServer);
-
-    //return httpServer;
-//}).listen(3000, function() {
-    //console.log("hey");
-//});
-
-//}
-
-var httpServer = http.Server(app);
-var io = require(__dirname + '/app/sockets/socketServer.js')(httpServer, sessionMiddleWare);
-httpServer.listen(PORT);
 
 //LOAD TESTING
 if(process.env.NODE_ENV === "loadtest") {
     var siege = require('siege');
     siege().on(PORT).concurrent(500).for(1000).times.withCookie.post('/login', {username: "billxiong24", pass:"pass"}).get('/home').attack();
 }
+
+
+function init(port) {
+    app.set('views', path.join(__dirname, '/views'));
+
+    var handlebars = expressHandlebars.create({ 
+        partialsDir  : [
+            __dirname + '/views/partials'    
+        ]
+    });
+
+
+    app.engine('handlebars', handlebars.engine);
+    app.set('view engine', 'handlebars');
+
+    // uncomment after placing your favicon in /public
+    //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+
+    app.use(helmet());
+    app.use(helmet.hidePoweredBy());
+    //TODO include csrf token in frontend input fields
+
+    app.use(logger('dev'));
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({ extended: false }));
+    app.use(cookieParser());
+    app.use(express.static(path.join(__dirname, 'public')));
+
+    var sessionMiddleWare = session({
+        secret: crypto.randomBytes(10).toString('hex'),
+        resave: true,
+        saveUninitialized: true,
+        //store : new RedisStore({
+            //host: process.env.HOST,
+            //port: process.env.REDIS_PORT || 6379,
+            //client: cache_store
+        //})
+    }); 
+    app.use(sessionMiddleWare);
+
+    app.use(function(req, res, next) {
+        if(req.session._csrf === undefined) {
+            req.session._csrf = crypto.randomBytes(20).toString('hex');
+        }
+        return next();
+    });
+
+    http.globalAgent.maxSockets = Infinity;
+
+    /* Set up server side socket*/
+
+    //handles back button problem of caching- reloads page every time to persist session
+    app.use(function(req, res, next) {
+        res.header('Cache-Control', 'no-cache, private, no-store, must-revalidate, max-stale=0, post-check=0, pre-check=0');
+        next();
+    });
+
+    app.use(flash());
+    app.use(passport.initialize());
+    app.use(passport.session());
+
+    require('./app/authentication/user-pass.js').passportAuth(passport);
+    require('./routes/index')(app, passport);
+
+    // catch 404 and forward to error handler
+    app.use(function(req, res, next) {
+        var err = new Error('Not Found');
+        err.status = 404;
+        next(err);
+    });
+
+    // error handler
+    app.use(function(err, req, res, next) {
+        // set locals, only providing error in development
+        res.locals.message = err.message;
+        res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+        // render the error page
+        res.status(err.status || 500);
+        res.render('error');
+    });
+
+    var httpServer = http.Server(app).listen(port, '0.0.0.0');
+    var io = require(__dirname + '/app/sockets/socketServer.js')(httpServer, sessionMiddleWare);
+
+    return httpServer;
+}
+
+
+
+//to disable clustering and ip hashing, comment below line and just call init(3000);
+require('./cluster_node.js')(cluster, http, init(0), PORT);
 
 module.exports = app;
---
+++ cluster_node.js
@@ -0,0 +1,46 @@
+function ipHash(ip, numProcesses) {
+    var num = "";
+    for(var i = 0; i < ip.length; i++) {
+        if(isNaN(ip[i])) {
+            continue;
+        }
+        num+=ip[i];
+    }
+
+    //doublewise bit not
+    return ~~num % numProcesses;
+}
+
+module.exports = function(cluster, http, httpHiddenServer, PORT) {
+
+    if(cluster.isMaster) {
+        var workers = [];
+        var numCPUs = require('os').cpus().length;
+
+        for(var i = 0; i < numCPUs; i++) {
+            workers[i] = cluster.fork();
+        }
+        
+        var httpServer = http.createServer();
+        httpServer.on('connection', function(conn) {
+            conn.pause();
+            if(conn.remoteAddress) {
+                var hashIndex = ipHash(conn.remoteAddress, numCPUs);
+                var worker = workers[hashIndex];
+                worker.send("sticky-session", conn);
+            }
+        });
+
+        httpServer.listen(PORT, '0.0.0.0');
+    }
+    else {
+
+        process.on('message', function(message, conn) {
+            if(message !== 'sticky-session') {
+                return;
+            }
+            httpHiddenServer.emit('connection', conn);
+            conn.resume();
+        });
+    }
+};
