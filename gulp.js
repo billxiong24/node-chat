da175c516139291e11af353ea48acaac51b29608
William Xiong <wwx@duke.edu>
2017-07-13 00:59:09 -0400

    set up and installed necessary gulp packages

---
+++ gulp.js
@@ -0,0 +1,14 @@
+var gulp = require('gulp');
+var uglify = require('gulp-uglify');
+var concat = require('gulp-concat');
+var gulp_jshint = require('gulp-jshint');
+var gulp_rename = require('gulp-rename');
+var gulp_handlebars = require('gulp-handlebars');
+
+//TODO precompile handlebars,
+//run jshint,
+//minify all serverside and clientside js
+//concat all serverside and clientside js
+//build and run project
+
+
